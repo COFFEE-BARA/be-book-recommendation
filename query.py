dbabb579badde49a5d301bd01e3f5f86b1a8cc06
raminicano <younju0804@sookmyung.ac.kr>
2024-02-21 01:54:40 +0900

    Refactor: docker apm 설정

--- query.py
+++ query.py
@@ -2,27 +2,27 @@
 from search_utils import search_with_embedding
 import json
 from open_ai import keyword_search
-from elasticapm.contrib.serverless.aws import capture_serverless
-import elasticapm
+# from elasticapm.contrib.serverless.aws import capture_serverless
+# import elasticapm
 import os
 
-SERVICE_NAME = os.getenv('SERVICE_NAME')
-SECRET_TOKEN = os.getenv('ELASTIC_APM_SECRET_TOKEN')
-SERVER_URL = os.getenv('ELASTIC_APM_LAMBDA_APM_SERVER')
-ENVIRONMENT = os.getenv('ENVIRONMENT')
+# SERVICE_NAME = os.getenv('SERVICE_NAME')
+# SECRET_TOKEN = os.getenv('ELASTIC_APM_SECRET_TOKEN')
+# SERVER_URL = os.getenv('ELASTIC_APM_LAMBDA_APM_SERVER')
+# ENVIRONMENT = os.getenv('ENVIRONMENT')
 
 
-# Elastic APM 구성
-client = elasticapm.Client({
-    'SERVICE_NAME': SERVICE_NAME,
-    'SECRET_TOKEN': SECRET_TOKEN,
-    'SERVER_URL': SERVER_URL,
-    'ENVIRONMENT' : ENVIRONMENT
-})
+# # Elastic APM 구성
+# client = elasticapm.Client({
+#     'SERVICE_NAME': SERVICE_NAME,
+#     'SECRET_TOKEN': SECRET_TOKEN,
+#     'SERVER_URL': SERVER_URL,
+#     'ENVIRONMENT' : ENVIRONMENT
+# })
 
 
 
-@capture_serverless()
+# @capture_serverless()
 def lambda_handler(event, context):
 
     try:
@@ -41,32 +41,32 @@ def lambda_handler(event, context):
             'body': json.dumps({'message': 'Invalid JSON in request body'})
         }
     
-    try:
-        # Elastic APM 트랜잭션 시작
-        client.begin_transaction("processing")
+    # try:
+    #     # Elastic APM 트랜잭션 시작
+    #     client.begin_transaction("processing")
         
-        match_queries = keyword_search(model_text)
-        result = search_with_embedding(model_text, match_queries)
+    #     match_queries = keyword_search(model_text)
+    #     result = search_with_embedding(model_text, match_queries)
 
-        # Elastic APM 트랜잭션 종료
-        client.end_transaction('process_event', 'success')
+    #     # Elastic APM 트랜잭션 종료
+    #     client.end_transaction('process_event', 'success')
     
-    except Exception as e:
-        client.capture_exception()  # 예외 상황을 Elastic APM으로 보고
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'message': 'Error processing request'}),
-            'headers': {{
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': '*'
-            }}
-        }
+    # except Exception as e:
+    #     client.capture_exception()  # 예외 상황을 Elastic APM으로 보고
+    #     return {
+    #         'statusCode': 500,
+    #         'body': json.dumps({'message': 'Error processing request'}),
+    #         'headers': {{
+    #             'Content-Type': 'application/json',
+    #             'Access-Control-Allow-Origin': '*',
+    #             'Access-Control-Allow-Headers': 'Content-Type',
+    #             'Access-Control-Allow-Methods': '*'
+    #         }}
+    #     }
 
 
-    # match_queries = keyword_search(model_text)
-    # result = search_with_embedding(model_text, match_queries)
+    match_queries = keyword_search(model_text)
+    result = search_with_embedding(model_text, match_queries)
 
 
     return {
